5ddec40235cafb6cce0ef975b2b01e1e7ef800d7
louis.pires <louis.pires@levtron.co.za>
2018-10-24 14:36:32 +0200

    added index.html & made app work dynamically

--- fl-app.py
+++ fl-app.py
@@ -1,28 +1,85 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-from flask import request
-from flask_api import FlaskAPI
+
+from flask import Flask, render_template
+from time import sleep
+import threading
+import json
 import RPi.GPIO as GPIO
 
-LEDS = {"blue": 16}
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(LEDS["blue"], GPIO.OUT)
+try:
+    values = {"new": 7.5, "cur": 7.5}
 
-app = FlaskAPI(__name__)
+    LEDS = {"UP": 16, "DOWN": 40}
+    GPIO.setwarnings(False)    # Ignore warning for now
+    GPIO.setmode(GPIO.BOARD)   # Use physical pin numbering
+    GPIO.setup(LEDS["UP"], GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(LEDS["DOWN"], GPIO.OUT, initial=GPIO.LOW)
 
-@app.route('/', methods=["GET"])
-def api_root():
-    return {
-           "led_url": request.url + "led/(blue)/",
-      		 "led_url_POST": {"state": "(0 | 1)"}
-    			 }
-  
-@app.route('/led/<color>/', methods=["GET", "POST"])
-def api_leds_control(color):
-    if request.method == "POST":
-        if color in LEDS:
-            GPIO.output(LEDS[color], int(request.data.get("state")))
-    return {color: GPIO.input(LEDS[color])}
+    app = Flask(__name__)
 
-if __name__ == "__main__":
-    app.run()
+    def triggerRelay(e, thread):
+        tn = threading.currentThread().getName()
+
+        GPIO.output(LEDS[tn], GPIO.HIGH)
+        print('LED ON \n')
+
+        print(values["new"])
+
+        tt = int(values["new"] * 10)
+
+        print(range(tt))
+        
+        for t in range(tt):
+            print(t)
+            if tn == 'UP':
+                values["cur"] = values["cur"] + (0.1)
+            if tn == 'DOWN':
+                values["cur"] = values["cur"] - (0.1)
+            print(round(values["cur"], 1))
+            sleep(0.1)
+
+        GPIO.output(LEDS[tn], GPIO.LOW)
+        print('LED OFF \n')
+
+    @app.route('/')
+    def index():
+        return render_template('index.html', val=str(round(values["cur"], 1)))
+
+    @app.route("/<action>")
+    def action(action):
+        if action == 'UP':
+            e = threading.Event()
+            thread = threading.Thread(name='UP', target=triggerRelay, args=(e, 2))
+            thread.start()
+
+        if action == 'DOWN':
+            e = threading.Event()
+            thread = threading.Thread(name='DOWN', target=triggerRelay, args=(e, 2))
+            thread.start()
+
+        return "LED OFF \n"
+
+    @app.route("/<action>/<time>")
+    def actionTime(action, time):
+        values["new"] = float(time)
+
+        if action == 'UP':
+            print(time)
+            e = threading.Event()
+            thread = threading.Thread(name='UP', target=triggerRelay, args=(e, 2))
+            thread.start()
+
+        if action == 'DOWN':
+            e = threading.Event()
+            thread = threading.Thread(name='DOWN', target=triggerRelay, args=(e, 2))
+            thread.start()
+
+        return "LED OFF \n"
+
+    if __name__ == "__main__":
+        app.run(debug=True, host='0.0.0.0', port=80)
+
+except:
+    GPIO.output(LEDS["DOWN"], GPIO.LOW)
+    GPIO.output(LEDS["UP"], GPIO.LOW)
